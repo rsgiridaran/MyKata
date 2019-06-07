73a826f854b17cec39accb213d7273fd23e99b4a
Giridaran <rsgiridaran@gmail.com>
2019-06-07 07:28:11 +0200

    Refactored - added assertion for exception and the message

--- src/test/java/com/channels/kata/BerlinClockTest.java
+++ src/test/java/com/channels/kata/BerlinClockTest.java
@@ -1,12 +1,19 @@
 package com.channels.kata;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class BerlinClockTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testBerlinClockForNullInput() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Please enter valid input");
+
         new BerlinClock(null);
     }
 
