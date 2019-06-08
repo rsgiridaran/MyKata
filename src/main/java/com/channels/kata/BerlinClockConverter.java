e393d2799abad9e172eefb160569bbc66e6a0112
Giridaran <rsgiridaran@gmail.com>
2019-06-08 08:16:07 +0200

    Refactor - extracted constanst for light color and number of lights in a row

--- src/main/java/com/channels/kata/BerlinClockConverter.java
+++ src/main/java/com/channels/kata/BerlinClockConverter.java
@@ -9,7 +9,11 @@ public class BerlinClockConverter {
 
     public static final int MAXIMUM_NUMBER_OF_LIGHTS_IN_HOUR_ROW = 4;
     public static final String NO_LIGHTS = "0";
-    public static final int CONSTANTS_FIVE = 5;
+    public static final int CONSTANT_FIVE = 5;
+    public static final String YELLOW_LIGHT = "Y";
+    public static final String RED_LIGHT = "R";
+    public static final int MAXIMUM_NUMBER_OF_LIGHTS_IN_MINUTE_FIRST_ROW = 11;
+    public static final int CONSTANT_TWO = 2;
     private BerlinClock berlinClock;
 
     public BerlinClockConverter(String digitalTime) {
@@ -17,16 +21,16 @@ public class BerlinClockConverter {
         berlinClock = processTime(localTime.getHour(), localTime.getMinute(), localTime.getSecond());
     }
 
-    private BerlinClock processTime(int hour,int minute, int seconds) {
+    private BerlinClock processTime(int hour, int minute, int seconds) {
         berlinClock = new BerlinClock();
-        berlinClock.setSecond((seconds % 2 == 0) ? "Y" : NO_LIGHTS);
-        berlinClock.setHourFirstRow(generateHourRowString(hour / CONSTANTS_FIVE,"R",MAXIMUM_NUMBER_OF_LIGHTS_IN_HOUR_ROW ));
-        berlinClock.setHourSecondRow(generateHourRowString(hour % CONSTANTS_FIVE, "R",MAXIMUM_NUMBER_OF_LIGHTS_IN_HOUR_ROW));
-        berlinClock.setMinutesFirstRow(generateHourRowString(minute / CONSTANTS_FIVE,"Y",11).replace("YYY","YYR"));
+        berlinClock.setSecond((seconds % CONSTANT_TWO == 0) ? YELLOW_LIGHT : NO_LIGHTS);
+        berlinClock.setHourFirstRow(generateHourRowString(hour / CONSTANT_FIVE, RED_LIGHT, MAXIMUM_NUMBER_OF_LIGHTS_IN_HOUR_ROW));
+        berlinClock.setHourSecondRow(generateHourRowString(hour % CONSTANT_FIVE, RED_LIGHT, MAXIMUM_NUMBER_OF_LIGHTS_IN_HOUR_ROW));
+        berlinClock.setMinutesFirstRow(generateHourRowString(minute / CONSTANT_FIVE, YELLOW_LIGHT, MAXIMUM_NUMBER_OF_LIGHTS_IN_MINUTE_FIRST_ROW).replace("YYY", "YYR"));
         return berlinClock;
     }
 
-    private String generateHourRowString(int numberOfLightsOnInRow,String lightColor,int manimumNumberOfLightsInRow) {
+    private String generateHourRowString(int numberOfLightsOnInRow, String lightColor, int manimumNumberOfLightsInRow) {
         StringBuilder rowStringBuilder = new StringBuilder();
         rowStringBuilder.append(StringUtils.repeat(lightColor, numberOfLightsOnInRow));
         rowStringBuilder.append(StringUtils.repeat(NO_LIGHTS, manimumNumberOfLightsInRow - numberOfLightsOnInRow));
